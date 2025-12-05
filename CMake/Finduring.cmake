a94449a83e6b94a468626dd594472846896a3c39
Yao Noel Achi <achi.noel@hotmail.com>
2025-12-05 08:09:19 -0500

    chore: update Photonlib to work with any liburing version

--- CMake/Finduring.cmake
+++ CMake/Finduring.cmake
@@ -1,4 +1,4 @@
-set(URING_VERSION 2.3)
+#set(URING_VERSION 2.3)
 
 find_path(URING_INCLUDE_DIRS liburing.h)
 
@@ -8,7 +8,7 @@ find_package_handle_standard_args(uring DEFAULT_MSG URING_LIBRARIES URING_INCLUD
 
 mark_as_advanced(URING_INCLUDE_DIRS URING_LIBRARIES)
 
-get_filename_component(URING_LIB_BASE ${URING_LIBRARIES} DIRECTORY)
-if (NOT EXISTS "${URING_LIB_BASE}/liburing.so.${URING_VERSION}")
-    message(FATAL_ERROR "Requires liburing ${URING_VERSION}. Install it to system or try -D PHOTON_BUILD_DEPENDENCIES=ON")
-endif ()
+#get_filename_component(URING_LIB_BASE ${URING_LIBRARIES} DIRECTORY)
+#if (NOT EXISTS "${URING_LIB_BASE}/liburing.so.${URING_VERSION}")
+#    message(FATAL_ERROR "Requires liburing ${URING_VERSION}. Install it to system or try -D PHOTON_BUILD_DEPENDENCIES=ON")
+    #endif ()
